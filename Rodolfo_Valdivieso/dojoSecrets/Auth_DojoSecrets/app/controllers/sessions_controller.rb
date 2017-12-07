547b9d39b7a597a811a6cb2f56138f0ebbed4518
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2017-12-07 10:59:19 -0600

    Dojo Secrets Authentication

---
+++ Rodolfo_Valdivieso/dojoSecrets/Auth_DojoSecrets/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def new
+  	return render 'new'
+  end
+
+  def log_in
+    # Log User In
+    # if authenticate true
+    @user = User.find_by_email(params[:Email]).try(:authenticate, params[:Password])
+    if @user
+    	session[:user_id] = @user.id
+    	return redirect_to UserShow_path @user.id
+    else
+    	flash[:failed] = ["Invalid Combination"]
+    	return redirect_to sessionNew_path
+    end
+  end
+  helper_method :log_in
+  
+  def destroy
+    # Log User out
+    session[:user_id] = nil
+    return redirect_to sessionNew_path
+  end
+
+  
+  
+end
---
+++ Rodolfo_Valdivieso/dojoSecrets/Auth_DojoSecrets/app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def show
+  	
+  	return render 'show'
+  end
+
+  def edit
+  end
+end
